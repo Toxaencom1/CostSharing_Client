8cba9520bf2ae485d7107a636ca303358bb0df60
Toxaencom1 <Toxaencom@gmail.com>
2024-03-01 20:52:34 +0500

    create client project to diplom work

---
+++ src/main/java/com/taxah/diplom_client/DiplomClientApplication.java
@@ -0,0 +1,13 @@
+package com.taxah.diplom_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiplomClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiplomClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/taxah/diplom_client/DiplomClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taxah.diplom_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiplomClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
